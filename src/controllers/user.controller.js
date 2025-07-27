49d3449700af67a2d72a3670ac7a05fa6434c428
shorya007 <kumarshourya7781@gmail.com>
2025-07-28 01:12:02 +0530

    user.controller.js
    ok

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req , res)=>{ //registerUser is wrapped inside asyncHandler, which means: If any error occurs inside the async function, it will be automatically caught and passed to the Express error handler.
     res.status(200).json({
-        message: "received"
+        message: "ok received successfully"
     })
 })
 
