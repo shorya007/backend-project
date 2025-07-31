dfea8730a61a8e3c56cd1c43024df5722b4c5268
Shaurya Kumar <shoryakumar77812@gmail.com>
2025-08-01 01:04:42 +0530

    commit it

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,6 @@
+//Jab frontend se koi request aati hai (jaise user login kare, register kare, profile update kare), to user.controller.js woh request receive karta hai.
+//Yeh file input data ko check karti hai (jaise email, password valid hai ya nahi), aur agar sab theek hai to aage service ya model ko call karti hai.
+
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import User from "../models/user.model.js";
@@ -18,21 +21,18 @@ const registerUser = asyncHandler( async (req , res)=>{ //registerUser is wrappe
     //8.)check for user creation 
     //9.)return response
 
-    const {fullName, email, username, password} = req.body
+    const {fullName, email, username, password} = req.body  //yaha pe req.body se extract kiye hai sare data points
     console.log("email", email);
     // if(fullName === ""){  //agar fullname empty hai toh
     //     throw new ApiError(400,"fullname is required")
     // }
 
-    if (
-        [fullName,email,username,password].some((field)=>
-            field?.trim() === ""
-        )
-    ) {
-        throw new ApiError(400,"All field are required")
-    }
+    if (!fullName || !email || !username || !password) {
+    throw new ApiError(400, "All fields are required");
+}
 
-    const existedUser=await User.findOne({
+
+    const existedUser=await User.findOne({  //check if user is already existed 
         $or: [{username}, {email}]  //mujhe ye find krke batao aisa ek user jo is email aur username ko match krta hai
     })
     
@@ -40,23 +40,33 @@ const registerUser = asyncHandler( async (req , res)=>{ //registerUser is wrappe
         throw new ApiError(409,"user with email or username already exists")
     }
 
+    // console.log(req.files);
+    console.log("req.body:", req.body);
+console.log("req.files:", req.files);
+    
+
     const avatarLocalPath = req.files?.avatar[0]?.path //Ye line ek safe way hai to access the uploaded avatar image file’s path
     //(?. :-it is called as optional chaining it ensures ki code crash na kare)
 
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-    if(!avatarLocalPath){
-        throw new ApiError(400,"Avatar file is required")
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
     }
 
-    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    if(!avatarLocalPath){  //avatar nhi mila toh error throw kr do
+        throw new ApiError(400,"Avatar file is required")
+    }
+ 
+    const avatar = await uploadOnCloudinary(avatarLocalPath)  //mil gaya toh cloudinary pe upload krdo
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if(!avatar){
+    if(!avatar){  // agar avatar nhi upload hua toh error de do
         throw new ApiError(400,"Avatar file is required")
     }
 
-    const newUser = await User.create({
+    const newUser = await User.create({  //agar sab kuch ho gaya hai toh object create kr do User.crate se value push kr diya aur value phuch gyi
         fullName,
         avatar: avatar.url,
         coverImage:coverImage?.url || "",
@@ -66,14 +76,14 @@ const registerUser = asyncHandler( async (req , res)=>{ //registerUser is wrappe
     })
 
     const createdUser = await User.findById(newUser._id).select(
-        "-password -refreshToken"
+        "-password -refreshToken"  //password aur refreshToken hata do jo ki receive value hai
     )
 
     if(!createdUser) {
-        throw new ApiError(500, "Something went wrogn while registering a user")
+        throw new ApiError(500, "Something went wrogn while registering a user")  //agar user nhi create hua toh eoor dedo
     }
 
-    return res.status(201).json(
+    return res.status(201).json(  //create ho gaya hai toh sb kr do
         new ApiResponse(200, createdUser, "user registered successfully")
     )
 })
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,4 @@
-//jo v method hum banate hain usko run karane mein route kam aata hai jab URL koi hit hoga tab run hona chaiye url ke sare sare jo routes hain uski ke liye banae hai url ke sb routes routes folder mein hee rkhte hain
+//jo v method hum banate hain usko run karane mein route kam aata hai jab URL koi hit hoga tab run hona chaiye url ke sare ke sare jo routes hain uski ke liye banae hai url ke sb routes routes folder mein hee rkhte hain
 
 //yaha user ke routes rkh rhe hain jaise registerUser , login etc
 
