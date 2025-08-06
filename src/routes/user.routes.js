db6aeb82a9c2d924a8f2989e674ca87c54a2a9a0
Shaurya Kumar <shoryakumar77812@gmail.com>
2025-08-06 16:57:14 +0530

    Add user account management routes for password change, user details update, and profile management

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,17 @@
 //yaha user ke routes rkh rhe hain jaise registerUser , login etc
 
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser,
+     logoutUser, 
+     registerUser,
+     refreshAccessToken, 
+     changeCurrentPassword, 
+     getCurrentUser, 
+     updateAccountDetails, 
+     updateUserAvatar, 
+     updateUserCoverImage, 
+     getWatchHistory, 
+     getUserChannelProfile } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"  //is upload ka use kaha krna hai middleware inject krne ke liye
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -30,5 +40,12 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)  //verifyJWT, logoutUser ke run hone se phle chlega
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/cuurent-user").get(verifyJWT, getCurrentUser)
+router.route("/update-Account-details").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router;
